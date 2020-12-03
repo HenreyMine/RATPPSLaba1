8ca3ff127ee2050cbd9605343778e337d0a16b52
Данил Шмырин <49958853+HenreyMine@users.noreply.github.com>
2020-12-03 20:51:40 +0400

    Add files via upload

---
+++ laba1/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var serializationType = Console.ReadLine();
+            var serializationObject = Console.ReadLine();
+
+            var isJson = serializationType == "Json";
+            ISerialize serializer;
+            if (isJson)
+            {
+                serializer = new JsonSerializator();
+            }
+            else
+            {
+                serializer = new XmlSerializator();
+            }
+            var input = serializer.Deserialize<Input>(serializationObject);
+
+            decimal sumResult = 0;
+            var sortedInputs = new List<decimal>();
+            foreach (var term in input.Sums)
+            {
+                sumResult += term;
+                sortedInputs.Add(term);
+            }
+            sumResult *= input.K;
+            var mulResult = 1;
+            foreach (var multiplier in input.Muls)
+            {
+                mulResult *= multiplier;
+                sortedInputs.Add(multiplier);
+            }
+            sortedInputs.Sort();
+            var output = new Output
+            {
+                SumResult = sumResult,
+                MulResult = mulResult,
+                SortedInputs = sortedInputs.ToArray(),
+            };
+            if (isJson)
+            {
+                Console.WriteLine(AddZeros(serializer.Serialize(output)));
+            }
+            else
+            {
+                Console.WriteLine(serializer.Serialize(output));
+            }
+        }
+
+        static public string AddZeros(string serializedObject)
+        {
+            var indexOfFirstComma = serializedObject.IndexOf(',');
+            var indexOfFirstPoint = serializedObject.IndexOf('.');
+            if (indexOfFirstPoint > indexOfFirstComma)
+            {
+                serializedObject = serializedObject.Substring(0, indexOfFirstComma) + ".0" + serializedObject.Substring(indexOfFirstComma);
+            }
+            var indexOfFirstSquareBracket = serializedObject.IndexOf('[');
+            var sortedInputs = serializedObject.Substring(indexOfFirstSquareBracket);
+            sortedInputs = sortedInputs.Substring(0, sortedInputs.Length - 2);
+            var sortedInputsList = sortedInputs.Split(',');
+            for (var i = 0; i < sortedInputsList.Length; i++)
+            {
+                if (!sortedInputsList[i].Contains('.'))
+                {
+                    sortedInputsList[i] += ".0";
+                }
+            }
+            sortedInputs = string.Join(",", sortedInputsList);
+            return serializedObject.Substring(0, indexOfFirstSquareBracket) + sortedInputs + "]}";
+        }
+    }
+}
---
+++ laba1/XmlSerializator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace laba1
+{
+    public class XmlSerializator : ISerialize
+    {
+        public T Deserialize<T>(string serializedString)
+        {
+            var xmlSerializer = new XmlSerializer(typeof(T));
+            var stringReader = new StringReader(serializedString);
+            return (T)xmlSerializer.Deserialize(stringReader);
+        }
+
+        public string Serialize<T>(T serializeObject)
+        {
+            var xmlSerializerNamespaces = new XmlSerializerNamespaces(new[] { XmlQualifiedName.Empty });
+            var xmlSerializer = new XmlSerializer(typeof(T));
+            var xmlWriteSettings = new XmlWriterSettings();
+            var stringWriter = new StringWriter();
+            xmlWriteSettings.OmitXmlDeclaration = true;
+            var xmlWriter = XmlWriter.Create(stringWriter, xmlWriteSettings);
+            xmlSerializer.Serialize(xmlWriter, serializeObject, xmlSerializerNamespaces);
+            return stringWriter.ToString();
+        }
+    }
+}
